ba9986af3f142561ca183aec462a65f2548a1d09
Andrey Zakharov <a.zakharov@i-sys.ru>
2016-10-05 16:20:15 +0400

    slide 5

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 var http = require("http");
 var url = require('url');
 
-function onRequest(request, response) {
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    var urlParsed = url.parse(request.url, true);
-    response.write("Привет, "+urlParsed.query.name+'!');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log("Request received.");
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        var urlParsed = url.parse(request.url, true);
+        response.write("Привет, " + urlParsed.query.name + '!');
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(2999);
 }
 
-http.createServer(onRequest).listen(2999);
+exports.start = start;
+
